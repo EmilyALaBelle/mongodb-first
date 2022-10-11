29ad672ad71f406b4f66724a40f96c2e51290f90
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-11 16:54:28 -0400

    without stopping

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient,ObjectId } from 'mongodb';
 
 import { uri } from "./credentials.js"
 
@@ -8,12 +8,35 @@ const moviesCollection = db.collection("movies")
 
 
 //console.log( await moviesCollection.findOne({}) )
-let query = { title: { $regex: /terminator/i } } //searches for terminator anywhere in the title i means case insensitive 
-let movieArray = await moviesCollection.find(query).limit(3).toArray()
+let query = { title: { $regex: /star wars/i } } //searches for terminator anywhere in the title i means case insensitive 
+let movieArray = await moviesCollection
+    .find(query)
+    //.limit(3)
+    .toArray() //make it into an array
 
-for(let i = 0; i < movieArray.length; i++){
+for (let i = 0; i < movieArray.length; i++) {
     console.log(movieArray[i].title)
 }
 // let firstMovie = movieArray[0]
 // console.log(firstMovie.title)
-console.log(`there are ${movieArray.length} movies`)
+
+
+
+//console.log(`there are ${movieArray.length} movies`)
+
+
+//add a new movie
+const newMovie = {
+    title: "The Boca Code story",
+    rating: "R",
+    genre: "Comedy",
+    releaseDate: "12/16/2022"
+}
+
+// const results = await moviesCollection.insertOne(newMovie)
+// console.log("Results of insert",results)
+
+const updateQuery = { _id: new ObjectId("6345ca9fbcb3ebbcba427051") }
+const update = {$set: {title: "The New Boca Code Story"}}
+const result = await moviesCollection.findOneAndUpdate(updateQuery,update);
+console.log(result)
